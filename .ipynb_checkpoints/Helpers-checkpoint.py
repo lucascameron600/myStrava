e192164a323c9fa50f96309f736e0aa85bfae0dc
lucascameron600 <lucas.cameron600@gmail.com>
2025-08-02 17:00:40 -0700

    Revert "smallchange"
    This reverts commit 65f12042be14e449a6e767368d49feabe9cfa9d1.

--- .ipynb_checkpoints/Helpers-checkpoint.py
+++
@@ -1,3 +0,0 @@
-
-def pounds_to_kilos(weight):
-    return weight * 0.453592
--- Person.py
+++ Person.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class Person:
-    def __init__(self, name, age, height, weight, RestHR, GPXfolder):
+    def __init__(self, name, age, height, weight, RestHR):
         self.name = name
         self.age = age
         self.height = height
